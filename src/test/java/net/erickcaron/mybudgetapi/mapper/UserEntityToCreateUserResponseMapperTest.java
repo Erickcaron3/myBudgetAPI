4bcf276260341265f8012620df50fee3923c1941
Erickcaron3 <68863461+Erickcaron3@users.noreply.github.com>
2023-06-07 18:25:17 +0200

    -Deleting not used tests

--- src/test/java/net/erickcaron/mybudgetapi/mapper/UserEntityToCreateUserResponseMapperTest.java
+++
@@ -1,9 +0,0 @@
-package net.erickcaron.mybudgetapi.mapper;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserEntityToCreateUserResponseMapperTest {
-
-}
